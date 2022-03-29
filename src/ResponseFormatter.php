8ea2e2e28bbf0afeafe8c8942fdd7e64080e0881
Joep Hoffland <joephoffland@gmail.com>
2022-03-29 22:22:23 +0200

    Possibility to format Guzzle requests and responses to HTTP messages.

---
+++ src/ResponseFormatter.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace GuzzleFormatter;
+
+use Psr\Http\Message\ResponseInterface;
+
+class ResponseFormatter extends Formatter
+{
+    public function http(ResponseInterface $response): string
+    {
+        return $this->formatHttp($response, $this->startLine($response));
+    }
+
+    private function startLine(ResponseInterface $response): string
+    {
+        return sprintf('HTTP/%s %s %s',
+            $response->getProtocolVersion(),
+            $response->getStatusCode(),
+            $response->getReasonPhrase()
+        );
+    }
+
+
+}
---
+++ tests/FormatterTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace GuzzleFormatter\Tests;
+
+use GuzzleFormatter\RequestFormatter;
+use GuzzleHttp\Psr7\Request;
+use PHPUnit\Framework\TestCase;
+
+class FormatterTest extends TestCase
+{
+    public function testEol()
+    {
+        $eol = "/r/n";
+
+        $requestFormatter = new RequestFormatter($eol);
+
+        $request = new Request(
+            'POST',
+            'https://www.google.com/search',
+            ['Content-Type' => 'application/json', 'Content-Length' => 18],
+            "{ \"type\": \"test\" }"
+        );
+
+        $formattedRequest = $requestFormatter->http($request);
+
+        $expectedFormat =
+            "POST /search HTTP/1.1" . $eol .
+            "Host: www.google.com" . $eol .
+            "Content-Type: application/json" . $eol .
+            "Content-Length: 18" . $eol .
+            $eol .
+            "{ \"type\": \"test\" }";
+
+        $this->assertSame($expectedFormat, $formattedRequest);
+    }
+}
